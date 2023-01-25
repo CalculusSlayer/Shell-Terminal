8b5477d5c685f67aed4d0a5bb4375ab695b99492
ktngo <ktngo@COE-CS-pc24.OU.AD3.UCDAVIS.EDU>
2023-01-25 14:46:50 -0800

    Small changes to sshell.c

--- sshell.c
+++ sshell.c
@@ -55,82 +55,85 @@ int main(void)
             *nl = '\0';
        
 
-        char **cmd_args = splitter(cmd);
-        //char *test_string = "echo hello world  grep Hello  output.txt";
-        //char **splitPipesTester = split_pipes(test_string);
+        //char **cmd_args = splitter(cmd);
         //Process testProcess = split_redirection(cmd);
+        char **pipe_strings = split_pipes(cmd);
+        Process processes[4] = { NULL, NULL, NULL, NULL };
+        for (int i = 0; i < 4; i++) {
+            if (pipe_strings[i] == NULL) {
+                    printf("Pipe %i is NULL\n", i);
+            }
+            printf("Pipe %i: %s\n", i, pipe_strings[i]);
+            processes[i] = split_redirection(pipe_strings[i]);
+        }        
         /* Builtin command */
-        if (!strcmp(cmd_args[0], "exit")) {
+        if (!strcmp(processes[0]->program, "exit")) {
             fprintf(stderr, "Bye...\n");
             fprintf(stderr, "+ completed '%s' [%d]\n", "exit", 0);
             break;
         }
         //TODO: Debug pwd. It is failing test.sh.
-        else if (!strcmp(cmd_args[0], "pwd")) {
+        else if (!strcmp(processes[0]->program, "pwd")) {
             // Maybe add error checking if getcwd() fails.
             getcwd(current_directory, sizeof(current_directory));
             printf("%s\n", current_directory);         
-            fprintf(stderr, "+ completed '%s' [%d]\n", cmd, 0);
+            fprintf(stderr, "+ completed '%s' [%d]\n", "pwd", 0);
             continue;
         }
-        else if (!strcmp(cmd_args[0], "cd")) {
-            //TODO: CHeck to see if provided directory is valid
-            
+        else if (!strcmp(processes[0]->program, "cd")) {
             int directory_not_found = 1;
-            dirp = opendir(cmd_args[1]);
-            
-            /*
-            dirp = opendir(".");
-            while ((dp = readdir(dirp)) != NULL) 
-            {
-                if (!strcmp(dp->d_name, cmd_args[1])) {
-                    directory_found = true;
-                    chdir(cmd_args[1]);
-                    break;
+            if (getLength(processes[0]->left_args) == 1) {
+                dirp = opendir(front(processes[0]->left_args));
+                
+                if (dirp) {
+                    chdir(front(processes[0]->left_args));
+                    directory_not_found = 0;
                 }
+                else
+                    fprintf(stderr, "Error: cannot cd into directory\n");
+                
+                //fprintf(stderr, "+ completed '%s %s' [%d]\n", processes[0]->program, processes[0]->left_args, directory_not_found);
+                closedir(dirp);
+                //continue;
             }
-            */
-            // Directory was successfully found
-            if (dirp) {
-                chdir(cmd_args[1]);
-                directory_not_found = 0;
-            }
-            else
-                fprintf(stderr, "Error: cannot cd into directory\n");
 
+            fprintf(stderr, "+ completed %s ", processes[0]->program);
 
-            fprintf(stderr, "+ completed '%s %s' [%d]\n", 
-                    cmd_args[0], cmd_args[1], directory_not_found);
-            closedir(dirp);
+            printLinkedList(stderr, processes[0]->left_args);
+            if (getLength(processes[0]->left_args) >= 1) printf(" ");
+            fprintf(stderr, "[%d]\n", directory_not_found);         
             continue;
-        }
-
-        /* Regular commands */
-        pid_t child_pid;
-        child_pid = fork();
-
-
-        if (child_pid == 0) {
-            //char **cmd_args = splitter(cmd);
-            //char *cmd_args[] = {cmd, "-l", NULL};
-            execvp(cmd_args[0], cmd_args);
-            perror("execv");
-            deallocator(&cmd_args);
-            exit(1);
-        }
-        else if (child_pid > 0) {
-            int child_status;
-            waitpid(child_pid, &child_status, 0);
-            // Printing to stderr instead of stdout now. Instructions
-            // said to print to stderr.
-            fprintf(stderr, "+ completed '%s' [%d]\n", cmd,  WEXITSTATUS(child_status));
-            deallocator(&cmd_args);
         }
         else {
-            perror("fork");
-            exit(1);
-        }
+    
+            /* Regular commands */
+            sshell_system(processes[0]);
+            /*
+            pid_t child_pid;
+            child_pid = fork();
 
+            if (child_pid == 0) {
+                //char **cmd_args = splitter(cmd);
+                //char *cmd_args[] = {cmd, "-l", NULL};
+                execvp(cmd_args[0], cmd_args);
+                perror("execv");
+                deallocator(&cmd_args);
+                exit(1);
+            }
+            else if (child_pid > 0) {
+                int child_status;
+                waitpid(child_pid, &child_status, 0);
+                // Printing to stderr instead of stdout now. Instructions
+                // said to print to stderr.
+                fprintf(stderr, "+ completed '%s' [%d]\n", cmd,  WEXITSTATUS(child_status));
+                deallocator(&cmd_args);
+            }
+            else {
+                perror("fork");
+                exit(1);
+            }
+            */
+        }
     }
 
     return EXIT_SUCCESS;
